3e38eac523aa830871480952f40c0b5135008760
ThomasC02 <thomascantrell02@gmail.com>
2024-09-12 12:15:45 -0400

    thomas

--- state.h
+++ state.h
@@ -13,27 +13,26 @@ typedef void action(void);
 // Define the format of a state struct.
 struct state {
   // Event Handlers
-  event_handler* manufacture_failed;
   event_handler* order_received;
-  event_handler* shipment_arrived;
-  event_handler* shipment_lost;
-  event_handler* manufacture_completed;
-  event_handler* valid_payment;
   event_handler* invalid_payment;
+  event_handler* valid_payment;
+  event_handler* manufacture_completed;
+  event_handler* manufacture_failed
+  event_handler* shippment_lost; 
+  event_handler* shippment_arrived;
 
   // Actions (Entry and Exit)
-  action* start_warranty;
-  action* refund;
-  action* charge_client;
-  action* dispatch_factory_lines;
-  action* shutdown_factory_lines;
   action* get_order_size;
   action* reset_attempts;
+  action* payment_rejected;
   action* get_payment_method;
   action* increment_attempts;
-  action* payment_rejected;
+  action* dispatch_factory_lines;
+  action* shutdown_factory_lines;
   action* update_stats;
+  action* charge_client;
   action* get_address;
+  action* start_warranty;
 
   // Entry and Exit actions
   action* entry_to;
