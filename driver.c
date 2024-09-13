b037f4fa2f91e841163668715af9e1f67e318373
Joshua Cassada <cassadjx@dukes.jmu.edu>
2024-09-12 22:14:36 -0400

    driver

--- driver.c
+++ driver.c
@@ -4,44 +4,64 @@
 
 state_t* current_state = &accepting;
 
-int main(void){
-    statsSetting current_event;
+int main(void) {
+    events current_event;
     int key;
 
+    // Print the initial state (if necessary)
     printStateName();
     current_state->entry_to();
 
-    while ((key=getchar()) != 'X'){
-        if (key == '\n') continue;
+    // Main event loop
+    while ((key = getchar()) != EOF) {
+        if (key == '\n') continue;  // Ignore newlines
 
-        current_event = INVALID_EVENT;
-        switch(key){
-            case 'c':
-                    current_event = accepting;
-                    puts("State: Accepting");
-                    break;
-            case 'C':
-                    current_event = CLOSED_DETECTED;
-                    puts("Event: CLOSED_DETECTED");
-                    break;
-            case 'o':
-                    current_event = OPEN_BUTTON_PRESSED;
-                    puts("Event: OPEN_BUTTON_PRESSED");
-                    break;
+        current_event = INVALID_EVENT;  // Default to invalid event
+
+        // Map character inputs to the corresponding events
+        switch (key) {
             case 'O':
-                    current_event = OPENED_DETECTED;
-                    puts("Event: OPENED_DETECTED");
-                    break;
-            case 'x':
-                    exit(0);
+                current_event = ORDER_RECEIVED;
+                puts("Event: Order received");
+                break;
+            case 'V':
+                current_event = VALID_PAYMENT;
+                puts("Event: Payment validated");
+                break;
+            case 'I':
+                current_event = INVALID_PAYMENT;
+                puts("Event: Payment rejected");
+                break;
+            case 'F':
+                current_event = MANUFACTURE_FAILED;
+                puts("Event: Factory failed");
+                break;
+            case 'C':
+                current_event = MANUFACTURE_COMPLETED;
+                puts("Event: Factory success");
+                break;
+            case 'R':
+                current_event = SHIPMENT_ARRIVED;
+                puts("Event: Shipment confirmed");
+                break;
+            case 'L':
+                current_event = SHIPMENT_LOST;
+                puts("Event: Shipment failed");
+                break;
+            case 'X':
+                current_event = INVALID_EVENT;
+                puts("Event: Terminate program");
+                break;
             default:
-                    puts("Event: INVALID_EVENT");
+                puts("Event: INVALID_EVENT");
         }
-        if (current_event != INVALID_EVENT){
+
+        if (current_event != INVALID_EVENT) {
             handle_event(current_event);
         }
     }
+
     printf("\n");
     fflush(stdout);
-    
+    return 0;
 }
